b914c78862ebf2740bd31f0e5b6743c4175837da
ryota-ohno <91549172+ryota-ohno@users.noreply.github.com>
2022-10-02 14:45:36 +0900

    Add files via upload

--- step2_twist/src/step2_twist2_0.py
+++ step2_twist/src/step2_twist2_0.py
@@ -64,7 +64,7 @@ def init_process(args):
                 for A2 in A2_list:
                     if A1==0 and A2==0:
                         continue
-                    a = a_ + 4.8 * n * abs(np.sin(np.radians(A1)))
+                    a = a_ #+ 4.8 * n * abs(np.sin(np.radians(A1)))
                     b = b_/np.cos(np.radians(A2))##aが映進面に垂直な方向　bが映進面に平行な方向　b方向には面間距離を保って分子を傾ける
                     init_para_list.append([np.round(a,1),np.round(b,1),theta,A1,A2,'NotYet'])
                                 
